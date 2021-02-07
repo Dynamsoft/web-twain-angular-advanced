10e5347cc046076c0270ed2ba6b15b169637c664
Tom Kent <tom@dynamsoft.com>
2021-02-07 14:12:39 +0800

    fix onload detecting issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,24 +30,26 @@ export class AppComponent implements OnInit {
     this.dwtService.bUseCameraViaDirectShow = this.bUseCameraViaDirectShow && !this.bNoInstall;
   }
   ngOnInit() {
-    let env = this.dwtService.runningEnvironment;
-    if (env.bMobile) {
-      this.bMobile = env.bMobile;
-      this.currentEnv += env.bChrome ? "Chrome " + env.strChromeVersion : "";
-      this.currentEnv += env.bFirefox ? "Firefox " + env.strFirefoxVersion : "";
-      this.currentEnv += env.bSafari ? "Safari" : "";
-    } else {
-      if (env.bWin)
-        this.bShowCameraOption = true;
-      this.currentEnv += env.isX64 ? "64bit " : "32bit ";
-      this.currentEnv += env.bWin ? "Windows, " : "";
-      this.currentEnv += env.bMac ? "macOS " + env.macOSX + " " + env.osVersion + ", " : "";
-      this.currentEnv += env.bLinux ? "Linux, " : "";
-      this.currentEnv += env.bChrome ? "Chrome " + env.strChromeVersion : "";
-      this.currentEnv += env.bFirefox ? "Firefox " + env.strFirefoxVersion : "";
-      this.currentEnv += env.bSafari ? "Safari" : "";
-      this.currentEnv += env.bIE ? "Internet Explorer" + env.strIEVersion : "";
-      this.currentEnv += env.bEdge ? "Edge" : "";
-    }
+    setTimeout(() => {
+      let env = this.dwtService.runningEnvironment;
+      if (env.bMobile) {
+        this.bMobile = env.bMobile;
+        this.currentEnv += env.bChrome ? "Chrome " + env.strChromeVersion : "";
+        this.currentEnv += env.bFirefox ? "Firefox " + env.strFirefoxVersion : "";
+        this.currentEnv += env.bSafari ? "Safari" : "";
+      } else {
+        if (env.bWin)
+          this.bShowCameraOption = true;
+        this.currentEnv += env.isX64 ? "64bit " : "32bit ";
+        this.currentEnv += env.bWin ? "Windows, " : "";
+        this.currentEnv += env.bMac ? "macOS " + env.macOSX + " " + env.osVersion + ", " : "";
+        this.currentEnv += env.bLinux ? "Linux, " : "";
+        this.currentEnv += env.bChrome ? "Chrome " + env.strChromeVersion : "";
+        this.currentEnv += env.bFirefox ? "Firefox " + env.strFirefoxVersion : "";
+        this.currentEnv += env.bSafari ? "Safari" : "";
+        this.currentEnv += env.bIE ? "Internet Explorer" + env.strIEVersion : "";
+        this.currentEnv += env.bEdge ? "Edge" : "";
+      }
+    }, 0);
   }
 }
